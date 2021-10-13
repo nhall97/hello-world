f43435dde8bb36fa0c2395c5a1b78a4d3ba8eaf5
Nathan <62209008+nhall97@users.noreply.github.com>
2021-10-13 11:44:20 +0100

    add button that generates text

--- packages/hello-world/public/client.js
+++ packages/hello-world/public/client.js
@@ -1,4 +1,6 @@
 const button = document.getElementById('myButton');
 button.addEventListener('click', function(e) {
-  console.log('Click clack');
+  let div = document.createElement('div');
+  div.innerHTML = '<p>Lorem Ipsum</p>';
+  document.body.appendChild(div);
 });
