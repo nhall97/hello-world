bba88004da561c378ec7f028b3e17391e5152425
Nathan <62209008+nhall97@users.noreply.github.com>
2021-10-13 20:26:44 +0100

    add id to client.js

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('myButton');
 button.addEventListener('click', function(e) {
   let div = document.createElement('div');
-  div.innerHTML = '<p>Lorem Ipsum</p>';
+  div.innerHTML = '<h2 id="Lorem">Lorem Ipsum</h2>';
   document.body.appendChild(div);
 });
