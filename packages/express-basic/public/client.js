e226de9177ad807cb825a44428161e3673df5944
Nathan <62209008+nhall97@users.noreply.github.com>
2021-10-13 18:34:52 +0100

    remove noise from repo

--- packages/express-basic/public/client.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('myButton');
-button.addEventListener('click', function(e) {
-  let div = document.createElement('div');
-  div.innerHTML = '<p>Lorem Ipsum</p>';
-  document.body.appendChild(div);
-});
