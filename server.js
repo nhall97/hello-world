9fbf77478d6db6f7486d91f49bc0300bbe5da7f6
Nathan <62209008+nhall97@users.noreply.github.com>
2021-10-13 20:26:14 +0100

    cleanup server.js

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 
 var path = require('path');
-// App
 const app = express();
-
-//https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error
 
 // Set port
 const PORT = process.env.PORT || 3000;
@@ -19,5 +16,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
-
-// Server
